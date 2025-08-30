6135f47d6a66bd289049bbb4671a3b03ef83fc14
Manoj-J-Workshop <manojj91298@gmail.com>
2025-08-30 14:58:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function stringChop(str, size) {
   // your code here
+	let chunks = [];
+  // let x = 0;
+
+  for(let i=0;i<str.length;i = i+ size){
+    let strsub = str.slice(i,i+size);
+    chunks.push(strsub);
+    // x++;
+  }
+
+  return chunks;
 }
+
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
